d386a39ed876b3583e19c2e192882b6806079d19
HpJkR <m.hertereau@gmail.com>
2023-06-08 19:40:28 +0200

    create chat-box

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
-import './App.css'
+
+import Chat from './components/Chat'
+
+import './App.scss'
+
 
 function App() {
   
 
   return (
     <>
-     
+     <Chat/>
     </>
   )
 }
