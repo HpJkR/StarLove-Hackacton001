8051dd44d9fc4c0c763dc44954c85adcec2f9d31
Gessmass <martinweilldrums@gmail.com>
2023-06-08 23:29:40 +0200

    Homepage done

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,49 +1,95 @@
+import React, { useState, useEffect } from "react";
+import "./HomePage.scss";
+
 const HomePage = () => {
+  const [buttonVisible, setButtonVisible] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setButtonVisible(true);
+    }, 5000);
+
+    const windowHeight = window.innerHeight;
+    const buttonPosition = windowHeight / 2;
+
+    setButtonVisible(true);
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+
+      if (scrollPosition > buttonPosition) {
+        setButtonVisible(false);
+      } else {
+        setButtonVisible(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      clearTimeout(timeout);
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div className="body">
-      <div id="background">
+    <>
+      <section>
         <div id="logo">
           <img src="src/assets/img/logo_rose.png" alt="logo" />
         </div>
+        <div className="body">
+          <div className="star star1"></div>
+          <div className="star star2"></div>
+          <div className="star star3"></div>
+          <div className="star star4"></div>
+          <div className="star star5"></div>
+          <div className="star star6"></div>
+          <div className="star star7"></div>
+          <div className="star star8"></div>
+          <div className="wrapper">
+            <div className="scroll-text">
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
 
-        <div className="wrapper">
-          <div className="scroll-text">
-            {/* <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br> */}
-            <h1>StarLove</h1>
-            <h2>scrolling Text</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-              dolorem perferendis provident natus adipisci eveniet quasi at
-              eaque, iste reprehenderit? Libero, ex consequatur? Alias,
-              molestiae natus, non ea facilis dolorem vitae incidunt sint
-              dignissimos error, est tenetur obcaecati neque ex!
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-              dolorem perferendis provident natus adipisci eveniet quasi at
-              eaque, iste reprehenderit? Libero, ex consequatur? Alias,
-              molestiae natus, non ea facilis dolorem vitae incidunt sint
-              dignissimos error, est tenetur obcaecati neque ex!
-            </p>
-            <div className="divButton">
-              <button
-                onClick={"http://xhtml.le-developpeur-web.com/img-xhtml.php"}
-              ></button>
+              <h1>Where to find love in space ?</h1>
+              <br></br>
+
+              <p>
+                In a distant galaxy, love spreads across the stars thanks to
+                "StarLove," the revolutionary dating application that allows you
+                to find your soulmate among the entire universe. Dive into a
+                cosmic adventure where interstellar encounters become a reality.
+                Don't let love be a distant galaxy. Download "StarLove" today
+                and let the Force guide you to your soulmate.
+              </p>
+              <br></br>
+              <p>
+                Don't let love be a distant galaxy. Download StarLove today and
+                let the Force guide you to your soulmate.
+              </p>
+              <div className="sabre">
+                <img src="src/assets/img/sabre_pink.png" />
+              </div>
+            </div>
+            <div className={`divButton ${buttonVisible ? "show" : ""}`}>
+              <button onClick={() => console.log("Button clicked!")}>
+                FIND
+              </button>
             </div>
           </div>
-          <div className="button_sabres"></div>
         </div>
-      </div>
-    </div>
+      </section>
+    </>
   );
 };
 
