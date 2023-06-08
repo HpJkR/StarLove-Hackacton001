dde1b75475139e2591b8afffedf0eb16563e3967
Johanna Hilaire <johannahilairepro@gmail.com>
2023-06-08 14:08:47 +0200

    structure in sections

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,17 +1,36 @@
 const Cards = () => {
   return (
-    <div className="peopleGlobal">
-      <div className="peopleCard">
-        <img
-          src="https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg"
-          alt="Luke Skywalker"
-        />
+    <div className="iphone">
+    
+        <section className="StarLove-logo">
+         <img className="settings"></img>
+         <h1>STARLOVE</h1>
+         <img className="dialog"></img>
+        </section>
+   
+
+        <section className="Profile">
+          <div className="character">
+          <img
+            src="https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg"
+            alt="Luke Skywalker"/>
+          </div>
+
         <div className="peopleName">
           <h2>Luke Skywalker</h2>
         </div>
-        <p>Alliance to Restore the Republic</p>
-      </div>
+
+      </section>
+         
+      <section className="footer-icons">
+        <img className="left-arrow"></img>
+        <img className="heart"></img>
+        <img className="right-arrow"></img>
+      </section>
+   
+      
     </div>
+   
   );
 };
 
