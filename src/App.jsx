02d688522812c4cfdad4f0889ab1d07a8d1a8a2e
Johanna Hilaire <johannahilairepro@gmail.com>
2023-06-08 23:45:27 +0200

    carrousel en place, boutons scss a styliser

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,39 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Cards from "./components/Cards";
 
 const App = () => {
   const [peoples, setPeoples] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
     fetch("https://miadil.github.io/starwars-api/api/all.json")
-      .then((res) => res.json()).then((res) => console.log(res) || setPeoples(res));
+      .then((res) => res.json())
+      .then((data) => {
+        setPeoples(data);
+      });
   }, []);
 
+  const handlePrevious = () => {
+    setCurrentIndex((prevIndex) => {
+      return prevIndex === 0 ? peoples.length - 1 : prevIndex - 1;
+    });
+  };
 
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => {
+      return prevIndex === peoples.length - 1 ? 0 : prevIndex + 1;
+    });
+  };
 
   return (
     <>
-      <Cards peoples={peoples} />
+      <Cards
+        peoples={peoples[currentIndex]}
+        handlePrevious={handlePrevious}
+        handleNext={handleNext}
+      />
     </>
-);
+  );
+};
 
-    }
 export default App;
