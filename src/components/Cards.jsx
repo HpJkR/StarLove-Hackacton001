cec3e3f645c0e73a17871275ec79432619602baf
HugoHtvl <hug.oo@hotmail.fr>
2023-06-09 03:52:20 +0200

    pull

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,36 +1,31 @@
 const Cards = () => {
   return (
     <div className="iphone">
-    
-        <section className="StarLove-logo">
-         <img className="settings"></img>
-         <h1>STARLOVE</h1>
-         <img className="dialog"></img>
-        </section>
-   
+      <section className="StarLove-logo">
+        <img className="settings"></img>
+        <h1>STARLOVE</h1>
+        <img className="dialog"></img>
+      </section>
 
-        <section className="Profile">
-          <div className="character">
+      <section className="Profile">
+        <div className="character">
           <img
             src="https://vignette.wikia.nocookie.net/starwars/images/2/20/LukeTLJ.jpg"
-            alt="Luke Skywalker"/>
-          </div>
+            alt="Luke Skywalker"
+          />
+        </div>
 
         <div className="peopleName">
           <h2>Luke Skywalker</h2>
         </div>
-
       </section>
-         
+
       <section className="footer-icons">
         <img className="left-arrow"></img>
         <img className="heart"></img>
         <img className="right-arrow"></img>
       </section>
-   
-      
     </div>
-   
   );
 };
 
